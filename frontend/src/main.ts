3f111eecd7a8a57224510d997a83df3e5fd0eaab
noahhsia <noah.hsia@qq.com>
2026-02-20 16:55:03 +0800

    Revert "前端基本框架"
    This reverts commit 16b5cac61b898300a456ba5a96ce24743d0bd212.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import { createPinia } from "pinia";
+import './assets/main.css'
 
-const app = createApp(App);
+import { createApp } from 'vue'
+import App from './App.vue'
 
-app.use(router);
-app.use(createPinia());
-
-app.mount("#app");
+createApp(App).mount('#app')
